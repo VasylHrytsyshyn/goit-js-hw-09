6b5425cc5bf969c8d60ca6b004de3dbfafa72e04
Vasyl Hrytsyshyn <matsjo19@gmail.com>
2022-02-06 17:08:52 +0200

    complete task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const refs = {
+    body: document.querySelector("body"),
+    btnStart: document.querySelector("button[data-start]"),
+    btnStop: document.querySelector("button[data-stop]"),
+};
+let timerId = null;
+
+refs.btnStart.addEventListener('click', () => {
+    timerId = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    refs.btnStart.disabled = true;
+});
+
+refs.btnStop.addEventListener('click', () => {
+    clearInterval(timerId);
+    refs.btnStart.disabled = false;
+});
